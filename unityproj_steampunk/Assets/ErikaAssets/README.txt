fd0a326576c3ba6648e3ebf6820dc8af49dbd19c
natalielh <nlehuenen@gmail.com>
2019-02-24 21:36:38 -0500

    Add entire project folder

---
+++ unityproj_steampunk/Assets/ErikaAssets/README.txt
@@ -0,0 +1,6 @@
+Sta.sh Uploads 27
+
+&nbsp;
+
+This stack downloaded from https://sta.sh/221g7ucygxo6
+
---
+++ unityproj_steampunk/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+
+    float horizontal_move = 0.0f;
+
+    public float run_speed = 40f;
+
+    bool jump = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        horizontal_move = Input.GetAxisRaw("Horizontal") * run_speed;
+
+        if (Input.GetButtonDown("Jump")) {
+
+            jump = true;
+
+        }
+
+
+	}
+
+    void FixedUpdate() {
+
+        controller.Move(horizontal_move * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+}
